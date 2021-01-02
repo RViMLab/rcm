242f8a1e07dad723d81839926c1512741eea5532
mhubii <m.huber_1994@hotmail.de>
2021-01-02 20:45:12 +0000

    added new minimizers

---
+++ rcom_impl/include/rcom_impl/damped_least_squares.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Eigen/Core>
+
+
+// damped least squares solutions: http://graphics.cs.cmu.edu/nsp/course/15-464/Spring11/handouts/iksurvey.pdf
+// trust region optimization https://en.wikipedia.org/wiki/Trust_region
+// trust region https://people.maths.ox.ac.uk/nakatsukasa/preprints/TRSrev2.pdf
+template <class MatT>
+Eigen::Matrix<typename MatT::Scalar, MatT::ColsAtCompileTime, MatT::RowsAtCompileTime>
+dampedLeastSquares(const MatT &mat, typename MatT::Scalar lambda = typename MatT::Scalar{1e-2}) // choose appropriately
+{
+    typedef typename MatT::Scalar Scalar;
+    auto svd = mat.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
+    const auto &singularValues = svd.singularValues();
+    Eigen::Matrix<Scalar, MatT::ColsAtCompileTime, MatT::RowsAtCompileTime> dampedSingularValuesInv(mat.cols(), mat.rows());
+    dampedSingularValuesInv.setZero();
+    for (unsigned int i = 0; i < singularValues.size(); ++i) {
+        dampedSingularValuesInv(i, i) = singularValues(i) / (singularValues(i)*singularValues(i) + lambda*lambda);
+    }
+    return svd.matrixV() * dampedSingularValuesInv * svd.matrixU().adjoint();
+};
--- rcom_impl/include/rcom_impl/pseudo_inverse.h
+++ rcom_impl/include/rcom_impl/pseudo_inverse.h
@@ -4,9 +4,16 @@
 
 
 // taken from https://gist.github.com/javidcf/25066cf85e71105d57b6
+// with singularity avoidance e.g. https://studywolf.wordpress.com/2013/10/10/robot-control-part-6-handling-singularities/
+//   - singular values s become small close to singularities 
+//       -> the inverse 1/s explodes and leads to uncontrolled motion
+//   - increasing the tolerance limits the value of 1/s
+// damped least squares solutions: http://graphics.cs.cmu.edu/nsp/course/15-464/Spring11/handouts/iksurvey.pdf
+// trust region optimization https://en.wikipedia.org/wiki/Trust_region
+// trust region https://people.maths.ox.ac.uk/nakatsukasa/preprints/TRSrev2.pdf
 template <class MatT>
 Eigen::Matrix<typename MatT::Scalar, MatT::ColsAtCompileTime, MatT::RowsAtCompileTime>
-pseudoinverse(const MatT &mat, typename MatT::Scalar tolerance = typename MatT::Scalar{1e-4}) // choose appropriately
+pseudoinverse(const MatT &mat, typename MatT::Scalar tolerance = typename MatT::Scalar{1e-2}) // choose appropriately
 {
     typedef typename MatT::Scalar Scalar;
     auto svd = mat.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
