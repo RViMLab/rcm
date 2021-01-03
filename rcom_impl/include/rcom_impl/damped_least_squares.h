4ba2cd07fe95308957ccd204581efb48fa984853
mhubii <m.huber_1994@hotmail.de>
2021-01-03 19:12:19 +0000

    updated damped solution

--- rcom_impl/include/rcom_impl/damped_least_squares.h
+++ rcom_impl/include/rcom_impl/damped_least_squares.h
@@ -6,10 +6,10 @@
 // damped least squares solutions: http://graphics.cs.cmu.edu/nsp/course/15-464/Spring11/handouts/iksurvey.pdf
 // trust region optimization https://en.wikipedia.org/wiki/Trust_region
 // trust region https://people.maths.ox.ac.uk/nakatsukasa/preprints/TRSrev2.pdf
-// adaptive lambda file:///tmp/mozilla_martin0/Wan2018_Article_AStudyOnAvoidingJointLimitsFor.pdf
+// adaptive lambda https://link.springer.com/article/10.1007/s12206-018-0240-7
 template <class MatT>
 Eigen::Matrix<typename MatT::Scalar, MatT::ColsAtCompileTime, MatT::RowsAtCompileTime>
-dampedLeastSquares(const MatT &mat, typename MatT::Scalar lambda = typename MatT::Scalar{1e-1}) // choose appropriately
+dampedLeastSquares(const MatT &mat, typename MatT::Scalar lambda = typename MatT::Scalar{1e-2}) // choose appropriately
 {
     typedef typename MatT::Scalar Scalar;
     auto svd = mat.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
